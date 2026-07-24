697f5f0e8214c410f4f1d4e42316c2aa669193a0
andreas <andreas.strauss@outlook.com>
2026-07-24 11:06:21 +0200

    fix: correct PV power gain to 0.0001 (0.1W to kW) and revert 32-bit registers to Big-Endian (1.0.8)

--- custom_components/foxess_smart/coordinator.py
+++ custom_components/foxess_smart/coordinator.py
@@ -14,8 +14,6 @@ from .modbus_client import (
     decode_s16,
     decode_u32_be,
     decode_s32_be,
-    decode_u32_le,
-    decode_s32_le,
 )
 
 _LOGGER = logging.getLogger(__name__)
@@ -68,47 +66,47 @@ class FoxESSUpdateCoordinator(DataUpdateCoordinator):
         data["grid_voltage_r"] = round(grid_vi[0] * 0.1, 1)
         data["grid_voltage_s"] = round(grid_vi[1] * 0.1, 1)
         data["grid_voltage_t"] = round(grid_vi[2] * 0.1, 1)
-        data["grid_current_r"] = round(decode_s32_le(grid_vi[3:5]) * 0.001, 3)
-        data["grid_current_s"] = round(decode_s32_le(grid_vi[5:7]) * 0.001, 3)
-        data["grid_current_t"] = round(decode_s32_le(grid_vi[7:9]) * 0.001, 3)
+        data["grid_current_r"] = round(decode_s32_be(grid_vi[3:5]) * 0.001, 3)
+        data["grid_current_s"] = round(decode_s32_be(grid_vi[5:7]) * 0.001, 3)
+        data["grid_current_t"] = round(decode_s32_be(grid_vi[7:9]) * 0.001, 3)
 
         # Block 5: Grid CT Power (39168 - 39169)
         grid_ct = self.client.read_registers(39168, 2)
-        grid_ct_val = round(decode_s32_le(grid_ct) * 0.001, 3)  # W -> kW
+        grid_ct_val = round(decode_s32_be(grid_ct) * 0.001, 3)  # W -> kW
         data["grid_ct_power"] = grid_ct_val
         data["grid_import_power"] = round(max(0.0, -grid_ct_val), 3)
         data["grid_export_power"] = round(max(0.0, grid_ct_val), 3)
 
         # Block 6: Load Power (39219 - 39226)
         load_p = self.client.read_registers(39219, 8)
-        data["load_power_r"] = round(decode_s32_le(load_p[0:2]) * 0.001, 3)
-        data["load_power_s"] = round(decode_s32_le(load_p[2:4]) * 0.001, 3)
-        data["load_power_t"] = round(decode_s32_le(load_p[4:6]) * 0.001, 3)
-        data["load_power_total"] = round(decode_s32_le(load_p[6:8]) * 0.001, 3)
+        data["load_power_r"] = round(decode_s32_be(load_p[0:2]) * 0.001, 3)
+        data["load_power_s"] = round(decode_s32_be(load_p[2:4]) * 0.001, 3)
+        data["load_power_t"] = round(decode_s32_be(load_p[4:6]) * 0.001, 3)
+        data["load_power_total"] = round(decode_s32_be(load_p[6:8]) * 0.001, 3)
 
         # Block 7: Battery Combined Power (39237 - 39238)
         bat_p = self.client.read_registers(39237, 2)
-        bat_p_val = round(decode_s32_le(bat_p) * 0.001, 3)  # W -> kW
+        bat_p_val = round(decode_s32_be(bat_p) * 0.001, 3)  # W -> kW
         data["battery_combined_power"] = bat_p_val
         data["battery_charge_power"] = round(max(0.0, -bat_p_val), 3)
         data["battery_discharge_power"] = round(max(0.0, bat_p_val), 3)
 
         # Block 8: Grid Power (39248 - 39253)
         grid_p = self.client.read_registers(39248, 6)
-        data["grid_power_r"] = round(decode_s32_le(grid_p[0:2]) * 0.001, 3)
-        data["grid_power_s"] = round(decode_s32_le(grid_p[2:4]) * 0.001, 3)
-        data["grid_power_t"] = round(decode_s32_le(grid_p[4:6]) * 0.001, 3)
+        data["grid_power_r"] = round(decode_s32_be(grid_p[0:2]) * 0.001, 3)
+        data["grid_power_s"] = round(decode_s32_be(grid_p[2:4]) * 0.001, 3)
+        data["grid_power_t"] = round(decode_s32_be(grid_p[4:6]) * 0.001, 3)
 
-        # Block 9: PV Power (39279 - 39282)
+        # Block 9: PV Power (39279 - 39282, 0.1W resolution -> kW, multiplier 0.0001)
         pv_p = self.client.read_registers(39279, 4)
-        data["pv1_power"] = round(decode_s32_le(pv_p[0:2]) * 0.001, 3)
-        data["pv2_power"] = round(decode_s32_le(pv_p[2:4]) * 0.001, 3)
+        data["pv1_power"] = round(decode_s32_be(pv_p[0:2]) * 0.0001, 3)
+        data["pv2_power"] = round(decode_s32_be(pv_p[2:4]) * 0.0001, 3)
         data["pv_power_total"] = round(data["pv1_power"] + data["pv2_power"], 3)
 
         # Block 10: PV Energy (39602 - 39605)
         pv_e = self.client.read_registers(39602, 4)
-        data["pv_production_total"] = round(decode_u32_le(pv_e[0:2]) * 0.1, 1)
-        data["pv_production_today"] = round(decode_u32_le(pv_e[2:4]) * 0.1, 1)
+        data["pv_production_total"] = round(decode_u32_be(pv_e[0:2]) * 0.1, 1)
+        data["pv_production_today"] = round(decode_u32_be(pv_e[2:4]) * 0.1, 1)
 
         # Block 11: Work Mode (49203)
         mode = self.client.read_registers(49203, 1)
